d583c192b29e6aeb38423f879f1853e41f33e50d
Bhoopendra Singh <bhoopendrasingh2306@gmail.com>
2024-03-13 16:25:26 +0530

    Updated Database Link

--- server/db/config.js
+++ server/db/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/settyl_data");
+mongoose.connect("MONGO_DB_LINK");
 mongoose.connection.once("open", () => {
   console.log("mongodb connected successfully");
 });
